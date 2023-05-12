4004d9cab2ef3aab1df080b2e258adec137a7c04
Mihail <mixxa2002@gmail.com>
2023-05-12 23:28:47 +0300

    modal PersonalCard

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -5,6 +5,9 @@ import '../styles/Table.css';
 import { useNavigate } from 'react-router-dom';
 import {fio} from '../components/FIO.js';
 import { FindFilter } from "../components/Filter.js";
+import Avatar from '../img/ava.png';
+import '../styles/PersonalCard.css';
+import { withTheme } from "@emotion/react";
 
 export default function UserPage(){
   const [col_1, setcol_1] = useState(false)
@@ -17,6 +20,30 @@ export default function UserPage(){
   const [tablehideNO, setTableHideNO] = useState(true)
 
   const[personcard, setPersoncard] = useState(false)
+
+  let uli = fio.filter((tem) => {return tem.id == 1 }).map((item) => {
+    return <ul className="div_2__ul">
+       <li>Номер партийного билета: <span className="span1">{item.numBilet}</span> </li>
+       <li>Дата выдачи билета: {item.dateStart}</li>
+       <li>Статус билета: {item.statusBilet}</li>
+       <li>Пол: {item.sex}</li>
+       <li>Дата рождения: {item.dateBirth}</li>
+       <li>Дата вступления: {item.dateStart}</li>
+       <li>Место вступления: {item.placeIssue}</li>
+       <li>Статус членства: {item.statusMember}</li>
+       <li>Место постановки на учет: {item.place}</li>
+       <li>Образование: {item.education}</li>
+       <li>Социальная категория: {item.socialGroup}</li>
+       <li>Сфера деятельности: {item.sphereActivity}</li>
+       <li>Место работы, должность: {item.placeJob}</li>
+       <li>Статус в партии: {item.statusPart}</li>
+       <li>Избирался ли депутатом: {item.deputat}</li>
+       <li>Контактная информация: {item.contact}</li>
+    </ul>
+  });
+
+
+
 
  function col (cols){
   if (cols == 'col1') {
@@ -71,7 +98,7 @@ export default function UserPage(){
   let tablePOO = fio.map(function(item,index) {
     return <tr key={item.id}>
       <td> {index+1}</td>
-       <td className="table__surname"><p onClick={()=>navigateTo('/PersonalCard')} className="table_span__surname">{item.surname}</p></td>
+       <td className="table__surname"><p  onClick={()=>{ setPersoncard(!personcard) /*navigateTo('/PersonalCard')*/}} className="table_span__surname">{item.surname}</p></td>
        <td>{item.name}</td>
        <td>{item.parent}</td>
        <td>{item.numBilet}</td>
@@ -135,6 +162,21 @@ let tablePause = fio.map(function(item,index) {
             <p>Отчет в Word</p>
             <p>Отчет в Excel</p>
           </div>
+
+
+          <div className={`PersonalCard_main ${personcard ? '' : 'hide' }`}>
+              <div className="div_1">
+              <p  onClick={()=>{setPersoncard(!personcard)}} className="cancel">X</p>
+              <img src={Avatar} className="div_1__Avatar" alt="no img" width='23.3mm' height='31mm'></img>
+              <div className="div_1__FIO">ТЕМОШЕНКО Кирилл Викторович</div>
+              </div>
+
+              <div className="div_2">
+              <h1>2</h1>
+              {uli}
+              </div>
+          </div>
+
 
         <div className="tables">
           <table className={`tablePOO ${tablehidePOO ? 'hide' : ''}`}> 
